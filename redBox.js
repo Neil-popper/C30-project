51c7151e32b4a1801ca594da033523b7bba6d9e5
Neil-popper <77514497+Neil-popper@users.noreply.github.com>
2021-04-06 14:26:15 -0500

    Add files via upload

---
+++ redBox.js
@@ -0,0 +1,35 @@
+class RedBox {
+
+    constructor(x, y, width, height) {
+        var options = {
+
+            isStatic: false,
+            restitution: 0.1,
+            frictions: 0.01,
+            density:0.02
+
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        strokeWeight(2);
+        fill("red");
+        rect(pos.x, pos.y, this.width, this.height);
+        if (this.body.speed<3) {    
+        } else{
+          this.visibility = this.visibility-5
+          World.remove(world, this.body)
+          push()
+         tint(0,this.visibility)
+         pop()
+        }
+
+    }
+}
